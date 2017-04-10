8d8ee8de084086892c80043badc92f264ab141a3
moszis <moszis@yahoo.com>
2017-04-10 14:37:33 -0400

    Gitignore

--- src/com/bots/kingsroad/PatternRecognition.java
+++ src/com/bots/kingsroad/PatternRecognition.java
@@ -218,7 +218,7 @@ public class PatternRecognition {
         	matchPercent = mmr.maxVal * 100;   
         }
         
-        DecimalFormat df = new DecimalFormat("####0.00");
+        //DecimalFormat df = new DecimalFormat("####0.00");
         //System.out.println("Match Percent: " + df.format(matchPercent)+"%");
         
         return matchPercent;
